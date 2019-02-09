bce6a89821e9ab15dffedfcec7bf97bac92e22e0
VenkataMangina <venkata.mangina@hyatt.com>
2019-02-09 09:12:56 -0600

    Added Reservation Model

---
+++ src/main/java/com/gpreservation/reservetable/ReservetableService.java
@@ -0,0 +1,14 @@
+package com.gpreservation.reservetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservetableService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservetableService.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/gpreservation/reservetable/models/Reservation.java
@@ -0,0 +1,12 @@
+package com.gpreservation.reservetable.models;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class Reservation {
+    private String GroupName;
+    private int numberOfPeople;
+    private Date dateOfReservation;
+    private Timestamp time;
+}
+
